2d7b4b3704ed29feb82b7ed238452bec419e8b2f
DannyCarter <carter519@gmail.com>
2019-10-07 15:13:16 -0400

    Created new cmpnts/add styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,53 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import HomePage from './components/HomePage.js'
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+// import RecipePage from './components/RecipePage.js';
+// import SingleUserRecipePage from './components/SingleUserRecipePage.js'
+// import CreateRecipe from './components/CreateRecipe.js'
+// import EditRecipe from './components/EditRecipe.js'
 
+// import UserList from './components/User.js'
 
-const getUserFromServer = () =>
-  fetch('/api/user/').then(res => res.json())
-
-export default class App extends React.Component {
-
-  state = {
-    user: [],
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div className="container test">
+        <header>
+          <Link to="/" className="navbar-brand">best of NETFLIX!</Link>
+        </header>
+        <nav className="nabar navbar-expand-lg navbar-light bg-light">
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <a href="/">Home</a>
+              <a href="">TV Series</a>
+              <a href="">Movies</a>
+              <a href="">Documentary</a>
+            </ul>
+          </div>
+        </nav>
+        <br/>
+        {/* I'm using react router to set the exact paths to my pages */}
+          <Route exact path="/" component={HomePage}/>
+          {/* <Route exact path="/single-user/:userId/recipes/createOneRecipe" component={CreateEntry}/> */}
+          {/* <Route exact path="/single-recipe/recipe._id/recipes" component={EditRecipe}/>
+          <Route exact path="/RecipePage" component={RecipePage}/>
+          <Route exact path="/single-user/:userId/recipes" component={SingleUserRecipePage}/> */}
+      </div>
+      </Router>
+    );
   }
-
-  componentDidMount = () => {
-    getUserFromServer().
-      then(user => {
-        this.setState({ user })
-      })
-  }
-  render = () => (
-    <div>
-      <ul>
-        {this.state.user.map(user => (
-          <li>
-            {user.userName} - {user.email}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-
 }
+
+// export default class CreateEntry extends Component {
+
+//   constructor(props) {
+//     super(props);
+
+//     this.onChange
+//   }
+// }
+
+export default App;
