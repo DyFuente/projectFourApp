a6a1e8af5fe9ce6d2e9f5675dc52e1d2853b5cf0
DannyCarter <carter519@gmail.com>
2019-10-09 10:40:27 -0400

    adding note form

--- client/src/components/SingleEntryPage.js
+++ client/src/components/SingleEntryPage.js
@@ -1,6 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
+class NewNoteForm extends React.Component {
+    state = {
+        name: "",
+        comment: "",
+        entry: `id`,
+    }
+    handleInput = (evnt) => {
+        let newNote = { ...this.state }
+        newNote[evnt.target.name] = evnt.target.value;
+        this.setState(newNote)
+    }
+    handleSubmit = (evnt) => {
+        evnt.preventDefault();
+        this.addNewNote(this.state)
+    }
+
+    addNewNote = (newNote) =>
+        fetch('/api/notes/',
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(newNote)
+            }).then(res => res.json())
+
+    render = () => (
+        <form onSubmit={this.handleSubmit} >
+            <input type="text"
+                name="name"
+                onChange={this.handleInput}
+                value={this.state.name}
+                placeholder="Enter name"
+            />
+            <br />
+        
+            <textarea 
+            rows="4" 
+            cols="50" 
+            type="text"
+            name="comment" 
+            onChange={this.handleInput}
+            value={this.state.comment}
+            form="usrform">
+                Enter text here...</textarea>
+            <br />
+            <input type="submit" value="Create Note" />
+        </form>
+    )
+}
+
 export default class getSingleEntry extends React.Component {
 
     state = {
@@ -31,6 +80,8 @@ export default class getSingleEntry extends React.Component {
     render() {
         return (
             //I'm using the render method to get all the recipes for one user
+            <div>
+
             <div className="singleEntry-container">
                 <div className="mediumSingleEntry-container">
                     <br />
@@ -38,6 +89,8 @@ export default class getSingleEntry extends React.Component {
                     <div className="medium-titleSingleEntry">{this.state.singleEntry.medium}</div>
                     <img src={this.state.singleEntry.art} />
                 </div>
+            </div>
+            <NewNoteForm />
             </div>
         )
     }
